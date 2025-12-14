74cc6d977f0bcc5952771d008d645945382e8a66
SebastianEscorcia <sebastianescorcia18@gmail.com>
2025-12-14 08:00:19 -0500

    Initial Commit

---
+++ src/BogotaMetroApp.jsx
@@ -0,0 +1,6 @@
+
+export const BogotaMetroApp = () => {
+  return (
+    <div>BogotaMetroApp</div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import {  BogotaMetroAppogotaMetroApp } from './BogotaMetroApp'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BogotaMetroAppogotaMetroApp/>
+  </StrictMode>,
+)
